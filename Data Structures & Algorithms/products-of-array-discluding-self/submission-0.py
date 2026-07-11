ec15bd917425e3957ef712a4addd965a8b7fa55f
Nelson Kwesi Xedzro <nelsonxedzro660@gmail.com>
2026-07-11 02:30:28 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        pref = [0] * n
+        suff = [0] * n
+
+        pref[0] = suff[n - 1] = 1
+        for i in range(1, n):
+            pref[i] = nums[i - 1] * pref[i - 1]
+        for i in range(n - 2, -1, -1):
+            suff[i] = nums[i + 1] * suff[i + 1]
+        for i in range(n):
+            res[i] = pref[i] * suff[i]
+        return res
+
+
+
+"""
+The key insight to remember whenever you see this problem, 
+don't think: How do I multiply everything except this number?"
+Instead think: I can get everything except this 
+number by multiplying everything on its left and everything on its right
+"""
